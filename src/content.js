5eac974fd2745e3c8be079dd3eb5adcefde8be68
LezdCS <63884731+LezdCS@users.noreply.github.com>
2021-02-19 18:59:44 +0100

    Battle Royale Streamer Mode

--- src/content.js
+++ src/content.js
@@ -10,23 +10,57 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
             document.body.removeEventListener('click', checkElementClicked);
             let compass = document.getElementsByClassName("compass__indicator");
 
-            function wait() {
+            function wait_game() {
                 if (compass.length !== undefined) {
                     try{
                         notes();
                     }catch (e){
-                        setTimeout(wait,100);
+                        setTimeout(wait_game,100);
                     }
                 }else{
-                    setTimeout(wait,100);
+                    setTimeout(wait_game,100);
                 }
             }
+
+            function wait_battle_royal_prelobby(){
+                let footer = document.getElementsByClassName("level-progress__footer");
+                if (footer.length !== undefined) {
+                    try{
+                        battle_royale_lobby();
+                    }catch (e){
+                        setTimeout(wait_battle_royal_prelobby,100);
+                    }
+                }else{
+                    setTimeout(wait_battle_royal_prelobby,100);
+                }
+            }
+
+            function wait_battle_royal_game(){
+                if (compass.length !== undefined) {
+                    try{
+                        battle_royale_game();
+                    }catch (e){
+                        setTimeout(wait_game,100);
+                    }
+                }else{
+                    setTimeout(wait_game,100);
+                }
+            }
+
             if(url[1] === "game" || url[1]=== "challenge"){
-                wait();
+                wait_game();
+            }
+            if(url[1] === "battle-royale" && url.length===2){
+                wait_battle_royal_prelobby();
+            }
+            if(url[1] === "battle-royale" && url.length===3){
+                wait_battle_royal_game();
             }
             break;
 
     }
+
+    //requests from the popup
     if(request.message.startsWith("changeTheme")){
         if(url[1] === "game" || url[1]=== "challenge"){
             const themeINFOS = JSON.parse(request.message.substring(11));
@@ -36,28 +70,143 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if(request.message.startsWith("changeFont")){
         if(url[1] === "game" || url[1]=== "challenge"){
             const fontsize = JSON.parse(request.message.substring(10));
-            size_changing(fontsize)
+            size_changing(fontsize);
         }
     }
 })
 
-function game_page_ready(){
-    const left_panel = document.getElementsByClassName("game-layout__controls");
-    const game_infos = document.getElementsByClassName("game-status__body");
-    if (left_panel.length === 0 && game_infos.length === 0){
-        setTimeout(game_page_ready,100);
-    }else{
-        notes();
+
+function battle_royale_lobby(){
+    const footer = document.querySelector(".level-progress__footer");
+    const divButtons = document.createElement("div");
+    footer.insertBefore(divButtons, footer.children[0]);
+
+    const blurring_photos = document.createElement("input");
+    blurring_photos.type="checkbox";
+    blurring_photos.id="blurring_photos";
+    blurring_photos.name="blurring_photos";
+    blurring_photos.value="blurring_photos";
+    blurring_photos.style.marginBottom="30px"
+    blurring_photos.onclick=function (){
+        if(blurring_photos.checked){
+            chrome.storage.local.set({'blurring_photos': true }, function() {});
+        }else{
+            chrome.storage.local.set({'blurring_photos': false }, function() {});
+        }
     }
+
+    const blurring_photos_label = document.createElement("label");
+    blurring_photos_label.style.marginRight="20px"
+    blurring_photos_label.innerHTML = "<label for=\"blurring_photos\">Blur avatars</label>";
+
+    const censor_nicknames = document.createElement("input");
+    censor_nicknames.type="checkbox";
+    censor_nicknames.id="censor_nicknames";
+    censor_nicknames.name="censor_nicknames";
+    censor_nicknames.value="censor_nicknames";
+    censor_nicknames.onclick=function (){
+        if(censor_nicknames.checked){
+            chrome.storage.local.set({'censor_nicknames': true }, function() {});
+        }else{
+            chrome.storage.local.set({'censor_nicknames': false }, function() {});
+        }
+    }
+
+    const censor_nicknames_label = document.createElement("label");
+    censor_nicknames_label.innerHTML = "<label for=\"censor_nicknames\">Censoring usernames</label>"
+
+    chrome.storage.local.get(['blurring_photos', 'censor_nicknames'], function(items) {
+        if(items.blurring_photos!==undefined){
+            if(items.blurring_photos){
+                blurring_photos.checked="true";
+            }
+        }
+        if(items.censor_nicknames!==undefined) {
+            if(items.censor_nicknames){
+                censor_nicknames.checked="true";
+            }
+        }
+    });
+
+    divButtons.appendChild(blurring_photos)
+    divButtons.appendChild(blurring_photos_label)
+    divButtons.appendChild(censor_nicknames)
+    divButtons.appendChild(censor_nicknames_label)
+}
+
+function battle_royale_game(){
+
+    function blurring_photos(){
+
+        let link = document.createElement("link");
+        let css_url = chrome.runtime.getURL("src/br_blur.css");
+        link.href = css_url;
+        link.type = "text/css";
+        link.rel = "stylesheet";
+        document.getElementsByTagName("head")[0].appendChild(link);
+    }
+
+    function censor_nicknames(){
+
+        let ListObserver = new MutationObserver(function (mutations) {
+            for (let mutation of mutations) {
+                if (mutation.type === 'childList') {
+                    if(mutation.target.className==="user-nick"){
+                        if (mutation.previousSibling?.data) {
+                            mutation.target.innerHTML = mutation.previousSibling.data;
+                        }
+                    }
+
+                    const list_of_classes = ['player-list', 'user-grid user-grid--normal', 'player-list__player-life', 'player-list__guess-flag', 'player-list__item','wrong-guesses','br-hud','gm-style','br-game-layout__panorama'
+                    ,'br-hud__timer ','life-icon life-icon--flag','popup__content']
+                    if(list_of_classes.includes(mutation.target.className)){
+
+                        let elements = document.querySelectorAll('.user-nick');
+                        for(let i=0; i<elements.length; i++){
+                            elements[i].innerHTML = "******"
+                        }
+
+                    }
+                }
+            }
+        });
+
+        let elements = document.querySelectorAll('.user-nick');
+        for(let i=0; i<elements.length; i++){
+            if(elements[i].parentNode.className!=="label-1"){
+                elements[i].innerHTML = "******"
+            }
+        }
+
+        ListObserver.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+
+    }
+
+    chrome.storage.local.get(['blurring_photos', 'censor_nicknames'], function(items) {
+        if(items.blurring_photos!==undefined){
+            if(items.blurring_photos){
+                blurring_photos();
+            }
+        }
+        if(items.censor_nicknames!==undefined) {
+            if(items.censor_nicknames){
+                censor_nicknames();
+            }
+        }
+    });
 }
 
 function size_changing(size){
-    console.log(size)
+
     const notearea = document.getElementById("textarea")
     notearea.style.fontSize=size+"px";
 }
 
 function colors_changing(backgroud, header, body){
+
     const game_infos = document.getElementsByClassName("game-statuses");
     for(let i = 0; i < game_infos.length; i++) {
         game_infos[i].style.backgroundColor = backgroud;
@@ -91,6 +240,7 @@ function checkElementClicked(e){
 }
 
 function notes(){
+
     chrome.storage.local.get(['themeBackground', 'themeHeader', 'themeBody','fontSizeArea'], function(items) {
         colors_changing(items.themeBackground, items.themeHeader, items.themeBody);
         size_changing(items.fontSizeArea);
@@ -98,6 +248,7 @@ function notes(){
 
     // make appear or disapear the notes when click on the notes button
     function openNotes(){
+
         if(divGlobalNote.style.visibility==="hidden"){
             divGlobalNote.style = "position: absolute; z-index: 4;\n" +
                 "visibility: visible; font-size: 15px; top:"+toppos+"; left:"+leftpos+";"
@@ -164,12 +315,12 @@ function notes(){
     document.body.addEventListener('click', checkElementClicked)
 
     header = document.getElementsByClassName("header")[0];
-
 }
 
 
 // function to drag the textarea
 function dragElement(elmnt) {
+
     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0, prevX=0, prevY=0;
     if (document.getElementById(elmnt.id + "header")) {
         // if present, the header is where you move the DIV from:
